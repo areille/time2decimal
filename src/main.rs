6cae8067f2cfc23f3e0120ac22e616f777d33bcf
areille <areille@tuta.io>
2020-11-30 19:19:45 +0100

    Write CLI and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+#[macro_use]
+extern crate clap;
+
+use std::process;
+
+pub fn convert(hours: u32, minutes: u32, seconds: u32) -> f32 {
+    hours as f32 + minutes as f32 * 1. / 60. + seconds as f32 * 1. / 3600.
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(time2decimal =>
+        (version: "0.1.0")
+        (author: "areille <areille@tuta.io>")
+        (about: "Converts time to decimal")
+        (@arg HOURS: +required "Hours")
+        (@arg MINUTES: +required "Minutes")
+        (@arg SECONDS: "Seconds")
+    )
+    .get_matches();
+
+    let hours = matches
+        .value_of("HOURS")
+        .unwrap()
+        .parse::<u32>()
+        .unwrap_or_else(|err| {
+            eprintln!("Problem parsing <HOURS> argument : {}", err);
+            process::exit(1);
+        });
+    let minutes = matches
+        .value_of("MINUTES")
+        .unwrap()
+        .parse::<u32>()
+        .unwrap_or_else(|err| {
+            eprintln!("Problem parsing <MINUTES> argument : {}", err);
+            process::exit(1);
+        });
+    let seconds = matches
+        .value_of("SECONDS")
+        .unwrap_or("00")
+        .parse::<u32>()
+        .unwrap_or_else(|err| {
+            eprintln!("Problem parsing <SECONDS> argument : {}", err);
+            process::exit(1);
+        });
+
+    println!(
+        "{}:{}:{} in decimal is {} hours",
+        hours,
+        minutes,
+        seconds,
+        convert(hours, minutes, seconds)
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convert_1() {
+        assert_eq!(convert(1, 0, 0), 1.0);
+    }
+    #[test]
+    fn convert_2() {
+        assert_eq!(convert(12, 20, 53), 12.348055);
+    }
 }
